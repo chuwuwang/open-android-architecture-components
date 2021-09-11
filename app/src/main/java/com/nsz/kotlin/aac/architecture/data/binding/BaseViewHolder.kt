14262a5cec5b9697e7d3a8e0bd6ee24c9af88783
zhou <chuwuwang@126.com>
2021-09-11 15:26:32 +0800

    feat：初始化项目

---
+++ app/src/main/java/com/nsz/kotlin/aac/architecture/data/binding/BaseViewHolder.kt
@@ -0,0 +1,8 @@
+package com.nsz.kotlin.aac.architecture.data.binding
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+class BaseViewHolder(var binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
+
+}
